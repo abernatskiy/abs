abcba86ded0b8b2c43d8659210324e9263b4a311
Anton Bernatskiy (ishigaki) <abernats@uvm.edu>
2016-04-13 23:41:09 -0400

    Network is set up for forward operations, with tests
    	modified:   abs.py
    	modified:   testAbs.py

--- abs.py
+++ abs.py
@@ -1,4 +1,5 @@
 import numpy as np
+from copy import deepcopy
 
 def _distance(x1, x2):
 	return np.linalg.norm(x2-x1)
@@ -17,22 +18,30 @@ class Abs(object):
 		self.labels = np.array(labels)
 		self.n_labels = len(np.unique(self.labels))
 
-		self.prototypes = np.random.random([self.n_prototypes, self.dim])
-		self.alphas = np.random.random(self.n_prototypes)
-		self.gammas = np.random.random(self.n_prototypes)
-		self.mem_degrees = np.zeros([self.n_prototypes, self.n_labels])
+		self.prototypes = np.random.random([self.n_prototypes, self.dim]) # prototypes - the surrogate nearest neighbor vectors
+		self.alphas = np.random.random(self.n_prototypes) # how certain we are in the membership degrees of the prototype
+		self.gammas = np.random.random(self.n_prototypes) # how far away should the influence of the prototype extend
+		self.mem_degrees = np.zeros([self.n_prototypes, self.n_labels]) # membership of the prototype in every label
 		for i in range(self.n_prototypes):
 			self.mem_degrees[i][np.random.randint(self.n_labels)] = 1.
-
 		return self
 
 	def _layer1(self, feat):
 		return [ alpha*np.exp(-1.*gamma*_distance(feat, prot)) for alpha,gamma,prot in zip(self.alphas, self.gammas, self.prototypes) ]
 
 	def _layer2(self, protoActivations):
-		return None
+		return [ (protoActivation*mem_degree, 1-protoActivation) for mem_degree,protoActivation in zip(self.mem_degrees, protoActivations) ]
+
+	def _layer3(self, protoEvidence):
+		curLabelEvid, curUncEvid = deepcopy(protoEvidence[0])
+		for labelEvid,uncEvid in protoEvidence[1:]:
+			curLabelEvid = curLabelEvid*(labelEvid + uncEvid*np.ones(self.n_labels)) + curUncEvid*labelEvid
+			curUncEvid = curUncEvid*uncEvid
+		return (curLabelEvid, curUncEvid)
 
 	def predict(self, predFeatures):
 		print('Abs predict called')
 		layer1 = [ self._layer1(f) for f in predFeatures ]
+		layer2 = [ self._layer2(pa) for pa in layer1 ]
+		layer3 = [ self._layer3(ev) for ev in layer2 ]
 		return [ None for f in predFeatures ]
--- testAbs.py
+++ testAbs.py
@@ -4,21 +4,60 @@ import numpy as np
 from abs import Abs
 
 class TestAbs(Abs):
+	def setRandomPrototypesMembersip(self):
+		self.mem_degrees = np.zeros([self.n_prototypes, self.n_labels])
+		for i in range(self.n_prototypes):
+			self.mem_degrees[i][np.random.randint(self.n_labels)] = 1.
+
+	def layer1OnPrototypes(self):
+		return np.array([ self._layer1(f) for f in self.prototypes ])
+
+	def layer2OnPrototypes(self):
+		return [ self._layer2(pa) for pa in self.layer1OnPrototypes() ]
+
+	def layer3OnPrototypes(self):
+		return [ self._layer3(e) for e in self.layer2OnPrototypes() ]
+
 	def testLayer1(self):
 		'''Checks if putting protypes through L1 yields alphas'''
-		prototypeImagesCoincide = np.diagonal([ self._layer1(f) for f in self.prototypes ]) == self.alphas
+		prototypeImagesCoincide = np.diagonal(self.layer1OnPrototypes()) == self.alphas
 		return prototypeImagesCoincide.all()
+
 	def testLayer2(self):
-		return False
+		'''Checks if items of evidence provided by the prototypes regarding themselves are pointing to the label of the prototype with alpha weight'''
+		self.setRandomPrototypesMembersip()
+		l2p = self.layer2OnPrototypes()
+		for i in range(self.n_prototypes):
+			labelsMasses, unknownMass = l2p[i][i]
+			if not (labelsMasses == self.mem_degrees[i]*self.alphas[i]).all() or unknownMass != 1-self.alphas[i]:
+				return False
+		return True
+
 	def testLayer3(self):
-		return False
+		'''For each classifier, checks if the classifier assigns all the mass to a the prototypes label if the label is certain (alpha=1, mem_degree=delta(i,label))'''
+		self.alphas = np.ones(self.n_prototypes)
+		self.setRandomPrototypesMembersip()
+		l3p = self.layer3OnPrototypes()
+		for i in range(self.n_prototypes):
+			labelMasses,uncMass = l3p[i]
+			if uncMass != 0. or np.argmax(labelMasses) != np.argmax(self.mem_degrees[i]):
+				return False
+		return True
 
 if __name__ == '__main__':
 	from main import loadData
 	features, labels, labelEnc = loadData('data/iris/iris.data')
-	testAbsCla = TestAbs(n_prototypes=100)
+	testAbsCla = TestAbs(n_prototypes=10)
 	testAbsCla.fit(features, labels)
-	if testAbsCla.testLayer1().all():
+	if testAbsCla.testLayer1():
 		print('Layer1 test success!')
 	else:
 		print('Layer1 test failed')
+	if testAbsCla.testLayer2():
+		print('Layer2 test success!')
+	else:
+		print('Layer2 test failed')
+	if testAbsCla.testLayer3():
+		print('Layer3 test success!')
+	else:
+		print('Layer3 test failed')
