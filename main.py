d9052bdb63b9bb6bc8ec469ddcbeb449705404b6
Anton Bernatskiy (ishigaki) <abernats@uvm.edu>
2016-05-10 03:44:10 -0400

    Plotting function done
    	modified:   main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2
 
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import LabelEncoder
 from abs import Abs
@@ -38,8 +39,41 @@ def getDecisionSpaceCrossSectionMap(classifier, fixDims, fixDimVals, freeDimsLim
 		curPos = [ fixDimVals[fixDims.index(dim)] if dim in fixDims else freeDimsLimits[freeDims.index(dim)][0] + resolution*linearIdxToGrid(i, dim) for dim in range(totalDims) ]
 		grid.append(curPos)
 
-	gridVals = classifier.predict(grid)
+	gridVals = classifier.predict(grid, rejectionCost=rejectionCost, newLabelCost=newLabelCost)
 	return gridVals.reshape(gridPts)
+
+def discrete_cmap(N, base_cmap=None):
+	"""Create an N-bin discrete colormap from the specified input map"""
+
+	# Note that if base_cmap is a string or None, you can simply do
+	#    return plt.cm.get_cmap(base_cmap, N)
+	# The following works for string, None, or a colormap instance:
+
+	base = plt.cm.get_cmap(base_cmap)
+	color_list = base(np.linspace(0, 1, N))
+	cmap_name = base.name + str(N)
+	return base.from_list(cmap_name, color_list, N)
+
+def plotDecisionSpaceCrossSectionMap(classifier, fixDims, fixDimVals, freeDimsLimits, resolution, rejectionCost=None, newLabelCost=None):
+	crossection = getDecisionSpaceCrossSectionMap(classifier, fixDims, fixDimVals, freeDimsLimits, resolution, rejectionCost=rejectionCost, newLabelCost=newLabelCost)
+	totalDims = len(fixDims) + len(freeDimsLimits)
+	freeDims = range(totalDims)
+	for dim in fixDims:
+		freeDims.remove(dim)
+	if len(freeDims) != 2:
+		raise ValueError('Only two-dimensional decision maps are supported, ' + str(len(freeDim)) + '-dimensional map implied by fixed dims')
+	if len(freeDimsLimits) != 2:
+		raise ValueError('Only two-dimensional decision maps are supported, ' + str(len(freeDim)) + '-dimensional map implied by limits')
+
+	ranges = [ [ start + resolution*float(i) for i in range(int((end-start)/resolution) + 1) ] for start,end in freeDimsLimits ]
+
+	x,y = np.meshgrid(ranges[0], ranges[1])
+#	cs = plt.contour(x.T,y.T,crossection, colors='k', nchunk=0)
+#	csf = plt.contourf(x.T,y.T,crossection, len(np.unique(crossection)), cmap=plt.cm.Paired)
+	plt.pcolor(x.T, y.T, crossection, cmap=discrete_cmap(len(np.unique(crossection)), plt.cm.jet))
+#	cb = plt.colorbar(ticks=np.unique(crossection), label='')
+	plt.xlabel('Dimension ' + str(freeDims[0]))
+	plt.ylabel('Dimension ' + str(freeDims[1]))
 
 if __name__ == '__main__':
 	irisDataFile = 'data/iris/iris.data'
@@ -59,3 +93,7 @@ if __name__ == '__main__':
 #	print('Got labels ' + str(labelEnc.inverse_transform(testLabelsKnn)) + ' from k-nn classifier (numericals ' + str(testLabelsKnn) + ')')
 	print('KNN: Got labels ' + str(testLabelsKnn))
 
+	plotDecisionSpaceCrossSectionMap(abscla, [0,1], [5.5, 3.0], [(0,6), (0,3)], 0.1, rejectionCost=0.5, newLabelCost=0.65)
+	cb = plt.colorbar(ticks=[-2,-1,0,1,2])
+	cb.set_ticklabels(['Novel', 'Reject', 'Iris Setosa', 'Iris Veriscolor', 'Iris Virginica'])
+	plt.show()
